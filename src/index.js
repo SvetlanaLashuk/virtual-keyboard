3cd69195853d4518fe11f00521cdefa70d9ef363
SvetlanaLashuk <svetlana.lashuk311@gmail.com>
2023-04-29 15:18:33 +0300

    feat: add language changing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import createKeyboard from './js/keyboard.js';
+import { langEnShow, langRuShow } from './js/styles.js';
 
 window.addEventListener('load', () => {
-  const language = localStorage.getItem('lang') || 'en';
+  let language = localStorage.getItem('lang') || 'en';
   createKeyboard(language);
+
+  function changeLanguage(event) {
+    if (event.shiftKey && event.ctrlKey) {
+      event.preventDefault();
+      if (language === 'en') {
+        language = 'ru';
+        document.adoptedStyleSheets = [langRuShow];
+      } else {
+        language = 'en';
+        document.adoptedStyleSheets = [langEnShow];
+      }
+    }
+  }
+
+  document.addEventListener('keydown', changeLanguage);
 });
